ed5aa182fa0b7ea211cc258985d72bcb2fe1eeb6
SiwarAttia <132559027+SiwarAttia@users.noreply.github.com>
2023-05-11 03:47:10 +0200

    task_9.bash

--- task_9.bash
+++ task_9.bash
@@ -1,5 +1,13 @@
-#!/bin/bash/
-cat /etc/passwd | while read file :
+#! /bin/bash
+# ifs.sh
+IFS=$':'
+while read -r  fd1 fd2 fd3 fd4 fd5 fd6 fd7
 do 
-  echo ${file}
-done
+echo "Username: $fd1"
+echo "Password: $fd2"  
+echo "Userid: $fd3" 
+echo "Groupid: $fd4" 
+echo "Userdesc: $fd5" 
+echo "Homedir: $fd6" 
+echo "Shell: $fd7"
+done < "/etc/passwd"
